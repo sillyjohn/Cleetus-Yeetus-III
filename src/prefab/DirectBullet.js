b0eeb54b82a2eea8a8b08194e2a4344a134b8f54
Ryan Heath <rheath@ucsc.edu>
2020-05-24 18:50:41 -0700

    Shooooot

--- src/prefab/DirectBullet.js
+++ src/prefab/DirectBullet.js
@@ -9,19 +9,20 @@ class DirectBullet extends Phaser.Physics.Arcade.Sprite{
         this.xSpeed = 0;
         this.ySpeed = 0;
         this.setSize(12, 12, true);
+        this.body.allowGravity = false;
     }
     
     create(){
         this.body.setSize(width/2);
         this.body.setMaxVelocity(this.MAX_X_VEL  , this.MAX_Y_VEL);
-        this.body.allowGravity = false;
+        
     }
     
     update(){
         this.x += this.xSpeed;
         this.y += this.ySpeed;
         this.born += 1;
-        if (this.born > 1800) {
+        if (this.born > 120) {
             this.setActive(false);
             this.setVisible(false);
         }
@@ -29,19 +30,26 @@ class DirectBullet extends Phaser.Physics.Arcade.Sprite{
 
     fire() {
         this.setPosition(this.scene.player.x, this.scene.player.y); // Initial position
-        this.direction = Math.atan( (this.scene.reticle.x-this.x) / (this.scene.reticle.y-this.y));
 
+        
         // Calculate X and y velocity of bullet to moves it from shooter to target
-        if (this.scene.reticle.y >= this.y) {
+        /*if (this.scene.reticle.y >= this.body.y) {
             this.xSpeed = this.speed * Math.sin(this.direction);
             this.ySpeed = this.speed * Math.cos(this.direction);
+            console.log(this.xSpeed);
+            console.log(this.ySpeed);
         }
         else {
             this.xSpeed = -this.speed * Math.sin(this.direction);
             this.ySpeed = -this.speed * Math.cos(this.direction);
-        }
+            console.log(this.xSpeed);
+            console.log(this.ySpeed);
+        }*/
 
-        this.rotation = this.scene.player.rotation; // angle bullet with shooters rotation
+        this.rotation = this.scene.player.rotation; // angle bullet with player rotation
+
+        this.scene.physics.velocityFromRotation(this.rotation, 2000, this.body.velocity);
+        console.log("");
         this.born = 0; // Time since new bullet spawned
     }
 }
