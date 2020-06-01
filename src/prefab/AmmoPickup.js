54b7cbf1c1e47ac18fc265d6077ba6cfd55b08dd
Ryan Heath <rheath@ucsc.edu>
2020-05-31 20:20:55 -0700

    resize

--- src/prefab/AmmoPickup.js
+++ src/prefab/AmmoPickup.js
@@ -5,12 +5,13 @@ class AmmoPickup extends Phaser.GameObjects.Sprite {
       //add object to existing scene
       this.setScale(1);
       this.setOrigin(0,0);
-      
+      Phaser.GameObjects.Image.call(this, scene, 0, 0, 'shells');
       this.scene.physics.world.enable(this);
       this.body.allowGravity = true;
       this.body.setSize(this.width, this.height);
       scene.add.existing(this);
       scene.physics.add.existing(this);
+      this.setScale(.1,.1);
     }
     setPos(xPos, yPos) {
       this.x = xPos;
